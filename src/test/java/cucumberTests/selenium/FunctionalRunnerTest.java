2eb027423776e5fb42d0bee5b6df8ff14eab4c23
ViTaLES <vitalbka.91@gmail.com>
2016-09-01 01:35:00 +0300

    commit5

--- src/test/java/cucumberTests/selenium/FunctionalRunnerTest.java
+++ src/test/java/cucumberTests/selenium/FunctionalRunnerTest.java
@@ -7,7 +7,7 @@ import cucumber.api.CucumberOptions;
 		//glue = "src/test/java/cucumberTests/selenium/glue/*Definitions",
 		monochrome = true,
 		plugin = {"pretty", "html:target/report/cucumber-report", "json:target/report/jsonReport/cucumber-report.json"},
-		features = {"src/test/resources/features/"}
+		features = {"src/test/resources/features/se"}
 		//,
 		//tags = {"@AreTestProductsAvailable?, @MainPage, @Login, @Registration, @RegistrationInCheckout, @ErrorPage, @Search, @ProductLineup,  @PDP, @Size, @Color, @Image, @Mini_cart, @WishlistFromPDP, @WishlistFromBasket, @WishlistFromCheckoutPage, @KontoFlow, @CODFlow, @FakturaFlow, @CreditCardFlow"}
 		//tags = {"@Checkout-"}
