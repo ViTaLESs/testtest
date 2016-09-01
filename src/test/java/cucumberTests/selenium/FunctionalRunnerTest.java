f7230eaca9986851546b6f87bebe40d8b52efe86
ViTaLES <vitalbka.91@gmail.com>
2016-09-01 03:24:57 +0300

    commit21

--- src/test/java/cucumberTests/selenium/FunctionalRunnerTest.java
+++ src/test/java/cucumberTests/selenium/FunctionalRunnerTest.java
@@ -8,10 +8,10 @@ import cucumber.api.CucumberOptions;
 		monochrome = true,
 		plugin = {"pretty", "html:target/report/cucumber-report", "json:target/report/jsonReport/cucumber-report.json"},
 		features = {"src/test/resources/features/se"}
-		//,
+		,
 		//tags = {"@AreTestProductsAvailable?, @MainPage, @Login, @Registration, @RegistrationInCheckout, @ErrorPage, @Search, @ProductLineup,  @PDP, @Size, @Color, @Image, @Mini_cart, @WishlistFromPDP, @WishlistFromBasket, @WishlistFromCheckoutPage, @KontoFlow, @CODFlow, @FakturaFlow, @CreditCardFlow"}
 		//tags = {"@Checkout-"}
-		/*tags = {"@a"}*/
+		tags = {"@a, @b"}
 )
 //TODO Size - delete comments T8,11,12
 public class FunctionalRunnerTest extends Fixture {
